b49e3beb78ce5f19a2e9073593b75897e57957bb
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-30 22:37:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  check_array = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |sub_key, sub_value|
+      sub_value.each do |name|
+      if !final_array[name]
+        final_array[name] = {}
+      end
+      if !final_array[name][key]
+           final_array[name][key] = []
+      end
+          final_array[name][key].push(sub_key.to_s)
+        end
+      end
+    end
 end
+  # write your code here!
